ffd1be72e89bff043964878c676ae1f2fd4f1a15
YAOUSTI <yaousti@gmail.com>
2020-01-19 17:48:34 +0100

    update upload controller

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Course;
 use App\Image;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 
 class ImageController extends Controller
 {
-    public function UploadImage(Request $request)
+    public function UploadImageCourse(Request $request)
     {
         request()->validate([
             'image' => 'required|image|mimes:' . implode(",", Config::get('imageable.mimes')) . '|max:' . Config::get('imageable.max_file_size')
@@ -21,9 +23,26 @@ class ImageController extends Controller
         if (isset($request->id)) {
             $image = new Image();
             $image->file_name = $image_name;
-            $image->imageable_type = "category";
-            $image->imageable_id = $request->id;
+            $course = Course::findOrFail($request->id);
+            $course->image()->save($image);
+        }
 
+        return response()->json(['message' => 'Image uploaded successfully']);
+    }
+    
+
+    public function UploadImageCategory(Request $request)
+    {
+        request()->validate([
+            'image' => 'required|image|mimes:' . implode(",", Config::get('imageable.mimes')) . '|max:' . Config::get('imageable.max_file_size')
+        ]);
+        $image_name = 'image-' . time() . '.' . request()->image->getClientOriginalExtension();
+
+        request()->image->move(public_path('images'), $image_name);
+
+        if (isset($request->id)) {
+            $image = new Image();
+            $image->file_name = $image_name;
             $categorie = Category::findOrFail($request->id);
             $categorie->image()->save($image);
         }
