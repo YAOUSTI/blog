0d18378b1eaeaabb1b6e7bd07e915f8fe04b2476
YAOUSTI <yaousti@gmail.com>
2020-01-19 17:49:03 +0100

    update database relationships

--- database/migrations/2020_01_17_203924_create_images_table.php
+++ database/migrations/2020_01_17_203924_create_images_table.php
@@ -16,13 +16,10 @@ class CreateImagesTable extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->bigIncrements('id')->unsigned();
 
-            $table->bigInteger('category_id')->unsigned();
             $table->string('file_name', 255)->nullable(false);
             $table->string('imageable_type', 255)->nullable(false);
             $table->bigInteger('imageable_id')->unsigned();
             $table->timestamps();
-
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
 
         });
     }
